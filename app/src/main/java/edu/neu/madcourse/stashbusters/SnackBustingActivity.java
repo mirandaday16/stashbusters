fa292821436a0ff092a90b24e81fd5e4c296dbbe
Clara <kim.clar@husky.neu.edu>
2020-11-23 18:53:08 -0500

    moved snack busting posts into cardview + recyclerview

--- app/src/main/java/edu/neu/madcourse/stashbusters/SnackBustingActivity.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/SnackBustingActivity.java
@@ -1,17 +1,12 @@
 package edu.neu.madcourse.stashbusters;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,25 +14,16 @@ import edu.neu.madcourse.stashbusters.model.SnackBustPost;
 import edu.neu.madcourse.stashbusters.model.SnackBustChoice;
 
 public class SnackBustingActivity extends AppCompatActivity {
-    private int currentPost;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    SnackRVAdapter adapter;
     private List<SnackBustPost> posts;
-    private ImageView snackImage;
-    private TextView questionText;
-    private TextView choiceOne;
-    private TextView choiceTwo;
-    private Handler imageHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getSupportActionBar().hide();
         setContentView(R.layout.activity_snack_busting);
-
-        currentPost=0;
-        snackImage = (ImageView) findViewById(R.id.snack_image);
-        questionText = (TextView) findViewById(R.id.question_text);
-        choiceOne = (TextView) findViewById(R.id.choice_one);
-        choiceTwo = (TextView) findViewById(R.id.choice_two);
 
         // Set up dummy data
         SnackBustChoice choice_one = new SnackBustChoice("smiley mouth");
@@ -50,39 +36,22 @@ public class SnackBustingActivity extends AppCompatActivity {
         posts = new ArrayList<>();
         posts.add(post);
 
-
-        // Fill layout with snack bust post data
-        SnackBustPost currPost = posts.get(currentPost);
-        questionText.setText(currPost.getTitle());
-        choiceOne.setText(currPost.getChoiceList().get(0).getText());
-        choiceTwo.setText(currPost.getChoiceList().get(1).getText());
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                setImageView(posts.get(currentPost).getPhotoUrl());
-            }
-        }).start();
-
+        recyclerView = findViewById(R.id.recycler_view);
+        layoutManager = new UnscrollableLinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new SnackRVAdapter(posts);
+        recyclerView.setAdapter(adapter);
     }
 
-    private void setImageView(String url) {
-        Bitmap bitmap = null;
-        try {
-            InputStream in = new
-                    URL(url)
-                    .openStream();
-            bitmap = BitmapFactory.decodeStream(in);
-        } catch (IOException e) {
-            e.printStackTrace();
+    // An unscrollable linear layout manager object
+    public class UnscrollableLinearLayoutManager extends LinearLayoutManager {
+        public UnscrollableLinearLayoutManager(Context context) {
+            super(context);
         }
 
-        final Bitmap finalBitmap = bitmap;
-        imageHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                snackImage.setImageBitmap(finalBitmap);
-            }
-        });
+        @Override
+        public boolean canScrollVertically() {
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/edu/neu/madcourse/stashbusters/SnackRVAdapter.java
@@ -0,0 +1,96 @@
+package edu.neu.madcourse.stashbusters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import edu.neu.madcourse.stashbusters.model.SnackBustPost;
+
+public class SnackRVAdapter extends RecyclerView.Adapter<SnackRVAdapter.SnackViewHolder> {
+    List<SnackBustPost> posts;
+
+    SnackRVAdapter(List<SnackBustPost> snacks){
+        posts = snacks;
+    }
+
+    static class SnackViewHolder extends RecyclerView.ViewHolder {
+        private ImageView snackImage;
+        private TextView questionText;
+        private TextView choiceOne;
+        private TextView choiceTwo;
+        private Handler imageHandler = new Handler();
+
+        SnackViewHolder (View itemView){
+            super(itemView);
+            snackImage = (ImageView) itemView.findViewById(R.id.snack_image);
+            questionText = (TextView) itemView.findViewById(R.id.question_text);
+            choiceOne = (TextView) itemView.findViewById(R.id.choice_one);
+            choiceTwo = (TextView) itemView.findViewById(R.id.choice_two);
+        }
+    }
+
+    @NonNull
+    @Override
+    public SnackViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_snack_busting, parent, false);
+        return new SnackViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final SnackViewHolder holder, final int position) {
+        // Fill layout with snack bust post data
+        SnackBustPost currPost = posts.get(position);
+        holder.questionText.setText(currPost.getTitle());
+        holder.choiceOne.setText(currPost.getChoiceList().get(0).getText());
+        holder.choiceTwo.setText(currPost.getChoiceList().get(1).getText());
+
+        // Load in the image in a separate thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                setImageView(holder, posts.get(position).getPhotoUrl());
+            }
+        }).start();
+    }
+
+    // Function to load in an image from an image URL.
+    private void setImageView(final SnackViewHolder holder, String url) {
+        Bitmap bitmap = null;
+        try {
+            InputStream in = new
+                    URL(url)
+                    .openStream();
+            bitmap = BitmapFactory.decodeStream(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final Bitmap finalBitmap = bitmap;
+        holder.imageHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                holder.snackImage.setImageBitmap(finalBitmap);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+        //return 1;
+    }
+}
