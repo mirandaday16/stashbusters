99db60e0169a68d3eeeaab041812456b8c0936c9
mirandadayadkins <mirandaday16@gmail.com>
2020-11-24 18:01:31 -0500

    created login dialog and new account activity to open from login page

--- app/src/main/java/edu/neu/madcourse/stashbusters/LoginActivity.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/LoginActivity.java
@@ -1,10 +1,20 @@
 package edu.neu.madcourse.stashbusters;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.LinkedList;
 
 import edu.neu.madcourse.stashbusters.databinding.LoginLandingScreenBinding;
 
@@ -16,15 +26,66 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.login_landing_screen);
 
         // Hide Action Bar - for aesthetic purposes only
         getSupportActionBar().hide();
 
         // Setting up binding instance and view instances
         binding = LoginLandingScreenBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
         final Button logInButton = binding.logInButton;
         final Button signUpButton = binding.signUpButton;
 
+        // Setting onClickListener for Log In Button - opens dialog box to enter username and password
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogBox(LoginActivity.this);
+            }
+        });
 
+        // Setting onClickListener for Sign Up Button - opens New Account Activity
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewAccount();
+            }
+        });
+
+        setContentView(view);
     }
+
+    /*
+    // Helper functions:
+    */
+
+    // Starts New Account activity
+    private void createNewAccount() {
+        Intent intent = new Intent(this, NewAccountActivity.class);
+        startActivity(intent);
+    }
+
+    // Opens a dialog box for user to input username and password
+
+    private void showDialogBox(Context context) {
+        final EditText usernameField = new EditText(context);
+        final EditText passwordField = new EditText(context);
+        final AlertDialog credentialsBox = new AlertDialog.Builder(context)
+                .setTitle("Log In")
+                .setMessage("Enter your username and password")
+                .setView(usernameField)
+                .setView(passwordField)
+                .setPositiveButton("Log In", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // TODO: Validate this data using Firebase
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        credentialsBox.show();
+    }
+
+
 }
---
+++ app/src/main/java/edu/neu/madcourse/stashbusters/NewAccountActivity.java
@@ -0,0 +1,18 @@
+package edu.neu.madcourse.stashbusters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import edu.neu.madcourse.stashbusters.databinding.NewAccountActivityBinding;
+
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    // Set up ViewBinding for the layout
+    private NewAccountActivityBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
