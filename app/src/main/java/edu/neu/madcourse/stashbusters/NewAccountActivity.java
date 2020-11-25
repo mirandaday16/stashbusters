a847334c524427534f31dd6a8f2033dc23848db9
mirandadayadkins <mirandaday16@gmail.com>
2020-11-25 14:52:34 -0500

    set up basic java file for new account activity

--- app/src/main/java/edu/neu/madcourse/stashbusters/NewAccountActivity.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/NewAccountActivity.java
@@ -3,6 +3,11 @@ package edu.neu.madcourse.stashbusters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 import edu.neu.madcourse.stashbusters.databinding.NewAccountActivityBinding;
 
 
@@ -16,6 +21,32 @@ public class NewAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_account_activity);
 
+        // Setting up binding instance and view instances
+        binding = NewAccountActivityBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        final EditText usernameField = binding.usernameInput;
+        final EditText passwordField = binding.passwordInput;
+        final ImageButton profilePicButton = binding.imageButton;
+        final EditText bioField = binding.bioInput;
+        final Button saveButton = binding.saveButton;
+
+        // Setting onClickListener for Profile Picture Button - opens camera (sensor) for user to choose a profile picture
+        profilePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Open camera for user to choose a profile picture
+            }
+        });
+
+        // Setting onClickListener for Save Button - gathers information entered in form and saves in Firebase
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Get all information from form and save to Firebase
+            }
+        });
+
+        setContentView(view);
 
     }
 }
