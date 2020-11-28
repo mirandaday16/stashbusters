01d33c9685b806ac95c31885f9269a4e7531972a
mirandadayadkins <mirandaday16@gmail.com>
2020-11-28 16:57:11 -0500

    login logic working, new account logic set up but not working yet

--- app/src/main/java/edu/neu/madcourse/stashbusters/NewAccountActivity.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/NewAccountActivity.java
@@ -1,20 +1,34 @@
 package edu.neu.madcourse.stashbusters;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import edu.neu.madcourse.stashbusters.databinding.NewAccountActivityBinding;
 
 
 public class NewAccountActivity extends AppCompatActivity {
+    private static final String TAG = NewAccountActivity.class.getSimpleName();
 
     // Set up ViewBinding for the layout
     private NewAccountActivityBinding binding;
+
+    // Set up a FirebaseAuth object to save the new account
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +44,9 @@ public class NewAccountActivity extends AppCompatActivity {
         final EditText bioField = binding.bioInput;
         final Button saveButton = binding.saveButton;
 
+        // Initializing auth object for Firebase account creation
+        mAuth = FirebaseAuth.getInstance();
+
         // Setting onClickListener for Profile Picture Button - opens camera (sensor) for user to
         // choose a profile picture
         profilePicButton.setOnClickListener(new View.OnClickListener() {
@@ -44,11 +61,46 @@ public class NewAccountActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Get all information from form and save to Firebase
+                String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+                if (password.length() > 16 || password.length() < 8) {
+                    Toast.makeText(NewAccountActivity.this,
+                            "Please choose a password between 8-16 characters.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.createUserWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(NewAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        startWorldFeedActivity(); // TODO: "user" needs to be passed to this somehow
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(NewAccountActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
             }
         });
 
         setContentView(view);
 
-    }
-}
+        }
+
+     /*
+    // Helper functions:
+    */
+
+// Starts World Feed Activity
+private void startWorldFeedActivity(){
+        // TODO: When World Feed activity exists, change this function to go  to World Feed
+        Intent intent=new Intent(this,PersonalProfileActivity.class);
+        startActivity(intent);
+        }
+        }
