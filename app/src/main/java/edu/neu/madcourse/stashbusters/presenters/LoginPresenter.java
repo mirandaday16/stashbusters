781c9d95794f939d7234e2aee5101724d76b3763
Zee Nguyen <zinguyenn@gmail.com>
2020-11-29 16:30:58 -0500

    refactor LoginActivity into MVP pattern

---
+++ app/src/main/java/edu/neu/madcourse/stashbusters/presenters/LoginPresenter.java
@@ -0,0 +1,77 @@
+package edu.neu.madcourse.stashbusters.presenters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import edu.neu.madcourse.stashbusters.NewAccountActivity;
+import edu.neu.madcourse.stashbusters.contracts.LoginContract;
+import edu.neu.madcourse.stashbusters.contracts.PersonalProfileContract;
+import edu.neu.madcourse.stashbusters.views.LoginActivity;
+import edu.neu.madcourse.stashbusters.views.PersonalProfileActivity;
+
+public class LoginPresenter implements LoginContract.Presenter {
+    private static final String TAG = NewAccountActivity.class.getSimpleName();
+
+    private LoginContract.MvpView mView;
+    private Context mContext;
+    private FirebaseAuth mAuth;
+    private String userId; // owner of the profile
+
+    public LoginPresenter(Context context) {
+        this.mContext = context;
+        this.mView = (LoginContract.MvpView) context;
+        mAuth = mView.getmAuth();
+    }
+
+    @Override
+    public void createNewAccount() {
+        Intent intent = new Intent(mContext, NewAccountActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    // Starts World Feed Activity
+    public void startWorldFeedActivity(String userId) {
+        // TODO: When World Feed activity exists, change this function to go  to World Feed
+        Intent intent = new Intent(mContext, PersonalProfileActivity.class);
+        intent.putExtra("userId", userId);
+        mContext.startActivity(intent);
+    }
+
+    @Override
+    public void validateNewUser(String username, String password) {
+        if (username.matches("") || password.matches("")) {
+            String msg = "Please enter a username and password";
+            mView.showToastMessage(msg);
+        } else {
+            mAuth.signInWithEmailAndPassword(username, password)
+                    .addOnCompleteListener((Activity) mView, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, send user to World Feed
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startWorldFeedActivity(user.getUid()); // TODO: "user" needs to be passed to this somehow
+                            } else {
+                                // If sign in fails, display a toast message to the user
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                mView.showToastMessage("Authentication failed.");
+                            }
+
+                        }
+                    });
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/neu/madcourse/stashbusters/views/LoginActivity.java
@@ -0,0 +1,123 @@
+package edu.neu.madcourse.stashbusters.views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import edu.neu.madcourse.stashbusters.NewAccountActivity;
+import edu.neu.madcourse.stashbusters.R;
+import edu.neu.madcourse.stashbusters.contracts.LoginContract;
+import edu.neu.madcourse.stashbusters.databinding.LoginLandingScreenBinding;
+import edu.neu.madcourse.stashbusters.presenters.LoginPresenter;
+
+public class LoginActivity extends AppCompatActivity implements LoginContract.MvpView {
+    private static final String TAG = NewAccountActivity.class.getSimpleName();
+
+    // Set up ViewBinding for the layout
+    private LoginLandingScreenBinding binding;
+
+    // Set up a FirebaseAuth object for login
+    private FirebaseAuth mAuth;
+
+    private LoginPresenter mPresenter;
+
+    private Button logInButton, signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initializing auth object for Firebase login
+        mAuth = FirebaseAuth.getInstance();
+
+        mPresenter = new LoginPresenter(this);
+
+        // Setting up binding instance and view instances
+        binding = LoginLandingScreenBinding.inflate(getLayoutInflater());
+        logInButton = binding.logInButton;
+        signUpButton = binding.signUpButton;
+
+        initListeners();
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in; if so, go to World Feed activity
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // TODO: send user to World Feed if signed in
+    }
+
+    @Override
+    public FirebaseAuth getmAuth() {
+        return mAuth;
+    }
+
+    @Override
+    public void showToastMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    /*
+        // Helper functions:
+        */
+    private void initListeners(){
+        // Setting onClickListener for Log In Button - opens dialog box to enter username and password
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogBox(LoginActivity.this, R.style.AlertDialogCustom);
+            }
+        });
+
+        // Setting onClickListener for Sign Up Button - opens New Account Activity
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPresenter.createNewAccount();
+            }
+        });
+    }
+
+    // Opens a dialog box for user to input username and password
+    private void showDialogBox(Context context, int themeResId) {
+        // Creating a Linear Layout containing EditText fields for entering username and password
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View credentialsView = factory.inflate(R.layout.login_dialog, null);
+        final EditText usernameField = (EditText) credentialsView.findViewById(R.id.usernameEntry);
+        final EditText passwordField = (EditText) credentialsView.findViewById(R.id.passwordEntry);
+
+        // Creating an Alert Dialog for the user to enter their username and password
+        final AlertDialog credentialsBox = new AlertDialog.Builder(context, themeResId)
+                .setView(credentialsView)
+                .setPositiveButton("Log In", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Pass username and password entered by the user into Firebase for validation
+                        String username = usernameField.getText().toString().trim();
+                        String password = passwordField.getText().toString().trim();
+                        mPresenter.validateNewUser(username, password);
+//                        }
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        credentialsBox.show();
+    }
+
+}
