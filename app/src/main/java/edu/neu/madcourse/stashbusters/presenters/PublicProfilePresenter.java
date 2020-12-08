4b689f180360e3403ca24add4c2c993f88b3e2aa
Zee Nguyen <zinguyenn@gmail.com>
2020-12-08 08:13:53 -0500

    fixed issue of hashmap and list casting

--- app/src/main/java/edu/neu/madcourse/stashbusters/presenters/PublicProfilePresenter.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/presenters/PublicProfilePresenter.java
@@ -18,6 +18,7 @@ import java.util.List;
 import edu.neu.madcourse.stashbusters.adapters.PostAdapter;
 import edu.neu.madcourse.stashbusters.R;
 import edu.neu.madcourse.stashbusters.contracts.PublicProfileContract;
+import edu.neu.madcourse.stashbusters.model.Comment;
 import edu.neu.madcourse.stashbusters.model.Post;
 import edu.neu.madcourse.stashbusters.model.StashPanelPost;
 import edu.neu.madcourse.stashbusters.model.StashSwapPost;
@@ -94,7 +95,11 @@ public class PublicProfilePresenter implements PublicProfileContract.Presenter {
                     postList.clear();
 
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        StashPanelPost post = dataSnapshot.getValue(StashPanelPost.class);
+                        // each datasnapshot is a post
+                        List<Comment> postComments = getPostCommentsList(snapshot);
+                        StashPanelPost post = (StashPanelPost) setPostData(dataSnapshot, "StashPanel");
+                        post.setComments(postComments);
+
                         postList.add(post);
                     }
                     postAdapter.notifyDataSetChanged();
@@ -114,7 +119,9 @@ public class PublicProfilePresenter implements PublicProfileContract.Presenter {
                 if (snapshot.exists()) {
 
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        StashSwapPost post = dataSnapshot.getValue(StashSwapPost.class);
+                        List<Comment> postComments = getPostCommentsList(snapshot);
+                        StashSwapPost post = (StashSwapPost) setPostData(dataSnapshot, "StashSwap");
+                        post.setComments(postComments);
                         postList.add(post);
                     }
                     postAdapter.notifyDataSetChanged();
@@ -130,6 +137,38 @@ public class PublicProfilePresenter implements PublicProfileContract.Presenter {
 
         Log.i(TAG, "getUserPostsData:success");
         mView.setPostListAdapter(postAdapter);
+    }
+
+    private List<Comment> getPostCommentsList(DataSnapshot postSnapShot) {
+        List<Comment> postComments = new ArrayList<>();
+        DataSnapshot commentsSnapshot = postSnapShot.child("comments");
+
+        for (DataSnapshot singleComment : commentsSnapshot.getChildren()) {
+            Comment comment = singleComment.getValue(Comment.class);
+            postComments.add(comment);
+        }
+
+        return postComments;
+    }
+
+    private Post setPostData(DataSnapshot postSnapShot, String postType){
+        Post post;
+        if (postType.equals("StashPanel")) {
+            post = new StashPanelPost();
+        } else {
+            post = new StashSwapPost();
+        }
+
+        long createdDate = (long) postSnapShot.child("createdDate").getValue();
+
+        post.setAuthorId(postSnapShot.child("authorId").getValue().toString());
+        post.setPhotoUrl(postSnapShot.child("photoUrl").getValue().toString());
+        post.setTitle(postSnapShot.child("title").getValue().toString());
+        post.setId(postSnapShot.child("id").getValue().toString());
+        post.setDescription(postSnapShot.child("description").getValue().toString());
+        post.setCreatedDate(createdDate);
+
+        return post;
     }
 
     @Override
