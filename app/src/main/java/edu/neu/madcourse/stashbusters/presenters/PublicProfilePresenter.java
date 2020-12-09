5458b2bd714a9837e846b5edaf4477eed0de2838
Zee Nguyen <zinguyenn@gmail.com>
2020-12-09 13:29:25 -0500

    follower count now update correctly

--- app/src/main/java/edu/neu/madcourse/stashbusters/presenters/PublicProfilePresenter.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/presenters/PublicProfilePresenter.java
@@ -3,9 +3,14 @@ package edu.neu.madcourse.stashbusters.presenters;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.neu.madcourse.stashbusters.R;
 import edu.neu.madcourse.stashbusters.contracts.PublicProfileContract;
@@ -13,22 +18,21 @@ import edu.neu.madcourse.stashbusters.contracts.PublicProfileContract;
 public class PublicProfilePresenter extends ProfilePresenter {
     private static final String TAG = PublicProfilePresenter.class.getSimpleName();
 
-//    private PublicProfileContract.MvpView mView;
     // targetUserId is the owner of this profile
     private String targetUserId, currentUserId;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference followRef;
+    private DatabaseReference followRef, userRef;
 
     public PublicProfilePresenter(Context context, String targetUserId) {
         super(context, targetUserId);
         this.targetUserId = targetUserId;
-//        this.mView = (PublicProfileContract.MvpView) context;
 
         mAuth = FirebaseAuth.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();
 
         followRef = FirebaseDatabase.getInstance().getReference().child("follows");
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
     }
 
     public void onFollowButtonClick(String buttonText) {
@@ -55,12 +59,34 @@ public class PublicProfilePresenter extends ProfilePresenter {
         followRef.child(currentUserId).child("following").child(targetUserId).setValue(true);
         // add current user to list of target user's follower
         followRef.child(targetUserId).child("followers").child(currentUserId).setValue(true);
+
+        updateUserCount("follow");
     }
 
     private void unfollowUser() {
         followRef.child(currentUserId).child("following").child(targetUserId).removeValue();
         followRef.child(targetUserId).child("followers").child(currentUserId).removeValue();
+
+        updateUserCount("unfollow");
     }
 
+    private void updateUserCount(final String followStatus) {
+        final DatabaseReference targetUserRef = userRef.child(targetUserId);
+        targetUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                long currentFollowCount = (long) snapshot.child("followerCount").getValue();
+                if (followStatus.equals("unfollow")){
+                    targetUserRef.child("followerCount").setValue(currentFollowCount - 1);
+                } else {
+                    targetUserRef.child("followerCount").setValue(currentFollowCount + 1);
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 }
