7cc639186dbfbd109d49383011288f89d831a592
Clara <kim.clar@husky.neu.edu>
2020-12-09 21:10:05 -0500

    code cleanup

--- app/src/main/java/edu/neu/madcourse/stashbusters/WorldFeedActivity.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/WorldFeedActivity.java
@@ -23,21 +23,7 @@ import edu.neu.madcourse.stashbusters.views.NavigationBarView;
 public class WorldFeedActivity extends AppCompatActivity implements WorldFeedContract.MvpView {
     private NavigationBarView navigationBarView;
     ContentActivityFeedBinding binding;
-    private static final String TAG = "Feed activfity ";
-
-    private List<Post> posts;
     WorldFeedPresenter mPresenter;
-    private RecyclerView postListRecyclerView;
-
-    private PostAdapter postAdapter;
-
-
-    //vars
-//    private ArrayList<String> mImageNames = new ArrayList<>();
-//    private ArrayList<String> mImages = new ArrayList<>();
-//    private ArrayList<String> mUserNames = new ArrayList<>();
-//    private ArrayList<String> mHeadlines  = new ArrayList<>();
-//    private ArrayList<String> mNumlikes = new ArrayList<>();
 
 
     @Override
@@ -47,7 +33,6 @@ public class WorldFeedActivity extends AppCompatActivity implements WorldFeedCon
 
         mPresenter = new WorldFeedPresenter(this);
 
-        Log.d(TAG, "On createL started ");
         // Navigation bar setup:
         binding = ContentActivityFeedBinding.inflate(getLayoutInflater());
         navigationBarView = binding.navigationBar;
@@ -72,46 +57,5 @@ public class WorldFeedActivity extends AppCompatActivity implements WorldFeedCon
             System.out.println("  there are no posts right now");
         }
     }
-
-//    private void initUserNames() {
-//        Log.d(TAG, "Usernames created ");
-//        mUserNames.add("Fergus");
-//        mUserNames.add("Miranda ");
-//        mUserNames.add("Clara ");
-//        mUserNames.add("Zee ");
-//
-//    }
-//    private void initHeadlines() {
-//        Log.d(TAG, "Headlines created " );
-//        mHeadlines.add("Testing post 1");
-//        mHeadlines.add("Testing post 2");
-//        mHeadlines.add("Testing post 3");
-//        mHeadlines.add("Testing post 4");
-//
-//    }
-//    private void initNumlikes() {
-//        Log.d(TAG, "Numlikes created");
-//        mNumlikes.add("43");
-//        mNumlikes.add("28");
-//        mNumlikes.add("25773");
-//        mNumlikes.add("274");
-//    }
-//
-//    private void initImages() {
-//        mImages.add("Pic of string");
-//        mImages.add("Pic of painting");
-//        mImages.add("Pic of pen");
-//        mImages.add("Pic of crayons");
-//    }
-
-//    private void initRecylcerView() {
-//        Log.d(TAG, "Recycler created " );
-//        RecyclerView recyclerView = findViewById((R.id.recycler_view_feed));
-//        FeedRecyclerAdapter adapter = new PostAdapter(this,posts);
-//        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//
-//    }
 
 }
--- app/src/main/java/edu/neu/madcourse/stashbusters/presenters/WorldFeedPresenter.java
+++ app/src/main/java/edu/neu/madcourse/stashbusters/presenters/WorldFeedPresenter.java
@@ -59,22 +59,15 @@ public class WorldFeedPresenter implements WorldFeedContract.Presenter {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-
-
                 //This takes us into the panel posts
                 DataSnapshot panelPosts = snapshot.child("panelPosts");
 
                 //Iterate through each user
                 for (DataSnapshot user : panelPosts.getChildren()){
-
                     //Iterate through each post and add to postslist
                     for (DataSnapshot userPost: user.getChildren()){
-                        //We know for sure that userPost contains all the correct info
-                        //Somewhere between it getting sent to setPostData and coming back, it gets transformed
-                        Log.d(TAG, userPost.toString() + " supposedly what  Ineed");
                         StashPanelPost post  =  (StashPanelPost) setPostData(userPost, "StashPanel");
                         posts.add(post);
-                        Log.d(TAG, post.toString() + " is this a problem also?");
                     }
                 }
 
@@ -82,22 +75,14 @@ public class WorldFeedPresenter implements WorldFeedContract.Presenter {
 
                 //Iterate through each user
                 for (DataSnapshot user : swapPosts.getChildren()){
-                    Log.d(TAG,user.toString() + " THis is the current user");
-
                     //Iterate through each post and add to postslist
                     for (DataSnapshot userPost: user.getChildren()){
-                        Log.d(TAG, userPost.toString() + "this is what ur supposed to be adding ");
-
                         StashSwapPost post  = (StashSwapPost) setPostData(userPost, "StashSwap");
-                        Log.d(TAG, " does it even get here");
                         posts.add(post);
-                        Log.d(TAG, posts.toString() + " this is what the posts should look like");
 
                     }
                 }
 
-
-// could it be something with implementation of
                 Collections.reverse(posts);
                 mView.setPosts(posts);
 
@@ -118,28 +103,16 @@ public class WorldFeedPresenter implements WorldFeedContract.Presenter {
         }else {
             post = new StashSwapPost();
         }
-        Log.d(TAG, post.toString() + " at the start this is what");
         long createdDate = (long) postSnapShot.child("createdDate").getValue();
 
-        Log.d(TAG, postSnapShot.child("authorId").getValue().toString() + " This is the current author ID");
 
         post.setAuthorId(postSnapShot.child("authorId").getValue().toString());
-
-
-        Log.d(TAG, postSnapShot.child("photoUrl").getValue().toString() + " This is the current photo url");
         post.setPhotoUrl(postSnapShot.child("photoUrl").getValue().toString());
-        Log.d(TAG, post.photoUrl + " and this is the value after equals");
-        Log.d(TAG, postSnapShot.child("title").getValue().toString() + " title currently");
         post.setTitle(postSnapShot.child("title").getValue().toString());
         post.setId(postSnapShot.child("id").getValue().toString());
         post.setDescription((postSnapShot.child("description").getValue().toString()));
 
         post.setCreatedDate(createdDate);
-
-
-        //Sending the post is just sending the post object we actually need the info contained within it
-
-        Log.d(TAG, post.photoUrl + " just before it gets sent");
 
         return post;
     }
